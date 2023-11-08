08e892aa9f55a66519a3dd91ccbc978c4e32e955
Chirra Sreeja <csreeja@huronconsultinggroup.com>
2023-11-08 15:44:02 +0530

    Added Email

--- Models/Person.cs
+++ Models/Person.cs
@@ -9,6 +9,8 @@ namespace DBFirstAPI.Models
         public int Id { get; set; }
         public string Name { get; set; } = null!;
         public int? Age { get; set; }
+
+        public string? Email {  get; set; }
            
         
 
