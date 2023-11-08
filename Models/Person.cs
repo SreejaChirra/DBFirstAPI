a0f050a115141ab65b6e577135c4fd65aaf1ae93
amruthak27 <kunkumudiamrutha02@gmail.com>
2023-11-08 15:31:15 +0530

    deleted email

--- Models/Person.cs
+++ Models/Person.cs
@@ -9,7 +9,7 @@ namespace DBFirstAPI.Models
         public int Id { get; set; }
         public string Name { get; set; } = null!;
         public int? Age { get; set; }
-        public string? Email { get; set; }      
+           
         
 
        
